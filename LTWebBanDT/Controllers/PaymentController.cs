659cd4e4378f072d126a650f4ca620462698646e
trungvu222 <vuthanhtrung385@gmail.com>
2023-03-27 15:30:31 +0700

    Updates 27/3

--- LTWebBanDT/Controllers/PaymentController.cs
+++ LTWebBanDT/Controllers/PaymentController.cs
@@ -3,6 +3,8 @@ using LTWebBanDT.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -49,6 +51,37 @@ namespace LTWebBanDT.Controllers
                 objLTWebBanHangEntities.SaveChanges();
             }
             return View();
+        }
+
+        public ActionResult Order(string Email, string Phone)
+        {
+            List<CartModel> cart = Session["cart"] as List<CartModel>;
+            string sMsg = "<html><body><table border='1'><caption>Thông tin đặt hàng</caption><tr><th>STT</th><th>Tên hàng</th><th>Số lượng</th><th>Đơn giá</th><th>Thành tiền</th></tr>";
+            int i = 0;
+            double tongtien = 0;
+            foreach (CartModel item in cart)
+            {
+                i++;
+                sMsg += "<tr>";
+                sMsg += "<td>" + i.ToString() + "</td>";
+                sMsg += "<td>" + item.Product + "</td>";
+                sMsg += "<td>" + item.Quantity.ToString() + "</td>";
+                sMsg += "<td>" + item.Quantity.ToString() + "</td>";
+                sMsg += "<td>" + String.Format("{0:#,###}", item.Quantity * item.Quantity) + "</td>";
+                sMsg += "<tr>";
+                tongtien += item.Quantity * item.Quantity;
+            }
+            //Gửi email cho khách hàng
+            sMsg += "<tr><th colspan='5'>Tổng cộng: "
+                + String.Format("{0:#,### vnđ}", tongtien) + "</th></tr></table>";
+            MailMessage mail = new MailMessage("vuthanhtrung385@gmail.com", Email, "Thông tin đơn hàng", sMsg);
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("vuthanhtrung385@gmail.com", "rpponiqakrmtmvsn");
+            mail.IsBodyHtml = true;
+            client.Send(mail);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
